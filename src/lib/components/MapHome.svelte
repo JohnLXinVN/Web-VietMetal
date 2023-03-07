d80f721348e4e6c2252ac6c17d833ac828874d1b
anhhm-john <anhhm.2003.work@gmail.com>
2023-03-07 13:47:00 +0700

    done content home page

---
+++ src/lib/components/MapHome.svelte
@@ -0,0 +1,79 @@
+<script>
+        import imgMap from '$lib/image/imgMap.png';
+    import imgCommaTop from '$lib/image/commaTop.png';
+    import imgCommaBottom from '$lib/image/commaBottom.png';
+</script>
+
+<!-- Begin: Map -->
+    <div class="bg-[#F3F5F8] py-[94px]">
+        <div class="max-w-screen-main mx-auto px-4 grid grid-cols-12 gap-2">
+            <div class="col-span-7">
+                <img src={imgMap} alt=""/>
+            </div>
+            <div class="col-span-5">
+                <div class="bg-bluePrimary font-semibold text-lg text-white p-7 relative">
+                    Công ty Cổ Phần Kim Khí Việt tự hào khi có mạng lưới hệ thống nhập khẩu đa dạng để phục vụ các khách hàng sản xuất trong và ngoài nước. Trong suốt thời gian qua, chúng tôi được sự tin tưởng từ những nhà cung cấp lẫn khách hàng và không ngừng phát tiển để trở thành công ty phân phối thép hàng đầu Việt Nam.
+                    <img src={imgCommaTop} alt="" class="absolute top-[-20px] left-[30px] w-[35px] h-[40px]"/>
+                    <img src={imgCommaBottom} alt="" class="absolute bottom-[-20px] right-[30px] w-[35px] h-[40px]"/>
+                </div>
+                <div class="mt-[50px] grid grid-row-5 gap-1 text-[#353945]">
+                    <div class="row-span-1 grid grid-cols-2 mt-2">
+                        <div class="col-span-1 flex">
+                            <div class="w-[14px] h-[14px] mt-2 bg-[#E40613] mr-3 rounded-full"></div>
+                            <p>Novolipetsk <br/>
+                                MMK <br/>
+                                Servestal</p>
+                        </div>
+                        <div class="col-span-1 flex">
+                            <div class="w-[14px] h-[14px] mt-2 bg-[#511B00] mr-3 rounded-full"></div>
+                            <p>POSCO <br />
+                                Dongbu <br/>
+                                Huyndai Steel</p>
+                        </div>
+                    </div>
+                    <div class="row-span-1 grid grid-cols-2 mt-2">
+                        <div class="col-span-1 grid grid-rows-2">
+                            <div class="flex row-span-1">
+                                <div class=" w-[14px] h-[14px] mt-2 bg-[#FF5512] mr-3 rounded-full flex items-center"></div>
+                                <p>ArcelorMittan</p>
+                            </div>
+                            <div class="flex row-span-1">
+                                <div class=" w-[14px] h-[14px] mt-2 bg-[#FFB000] mr-3 rounded-full flex items-center"></div>
+                                <p>FMS<br/>
+                                    Hoa Phat</p>
+                            </div>
+                        </div>
+                        <div class="col-span-1 flex ">
+                            <div class="w-[14px] h-[14px] mt-2 bg-[#511B00] mr-3 rounded-full flex items-center"></div>
+                            <p>R&K Trading<br/>
+                                Kobe Steel<br/>
+                                Nippon Steel<br/>
+                                JFE </p>
+                        </div>
+                    </div>
+                    <div class="row-span-1 grid grid-cols-2 mt-2">
+                        <div class="col-span-1 flex">
+                            <div class="w-[14px] h-[14px] mt-2 bg-[#00D172] mr-3 rounded-full flex items-center"></div>
+                            <p>BlueScope Steel</p>
+                        </div>
+                        <div class="col-span-1 flex ">
+                            <div class="w-[14px] h-[14px] mt-2 bg-[#DD10C0] mr-3 rounded-full flex items-center"></div>
+                            <p>Chunghung <br/> CSC <br/> Shang Hsing</p>
+                        </div>
+                    </div>
+                    <div class="row-span-1 grid grid-cols-1 mt-2">
+                        <div class="col-span-1 flex">
+                            <div class="w-[14px] h-[14px] mt-2 bg-[#322783] mr-3 rounded-full flex items-center"></div>
+                            <p>HBIS Group<br/>
+                                Baosteel<br/>
+                                Xi Wang Group<br/>
+                                Zhejiang Huada New Materials</p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Map -->
